33f9dad05bdf33138abe3d0767306df950876e9e
adamn89 <adam.nowak.it@gmail.com>
2021-01-20 20:07:02 +0100

    Update vetclinicdb.sql

--- db scripts/vetclinicdb.sql
+++ db scripts/vetclinicdb.sql
@@ -1,60 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Czas generowania: 20 Sty 2021, 19:20
--- Wersja serwera: 10.4.17-MariaDB
--- Wersja PHP: 8.0.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Baza danych: `vetclinicdb`
---
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `appointment`
---
-
-CREATE TABLE `appointment` (
-  `id` bigint(20) NOT NULL,
-  `appointment_date_and_time` datetime DEFAULT NULL,
-  `customer_id` bigint(20) DEFAULT NULL,
-  `doctor_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Zrzut danych tabeli `appointment`
---
-
-INSERT INTO `appointment` (`id`, `appointment_date_and_time`, `customer_id`, `doctor_id`) VALUES
-(1, '2021-01-03 02:35:58', 1, 1),
-(2, '2021-01-03 03:35:00', 2, 1);
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `customer`
---
-
-CREATE TABLE `customer` (
-  `id` bigint(20) NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `pin` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 --
 -- Zrzut danych tabeli `customer`
 --
@@ -64,17 +7,6 @@ INSERT INTO `customer` (`id`, `first_name`, `last_name`, `pin`) VALUES
 (2, 'Piotr', 'Nowak', 202),
 (3, 'Krzysztof', 'Krawczyk', 303);
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `doctor`
---
-
-CREATE TABLE `doctor` (
-  `id` bigint(20) NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `doctor`
@@ -84,59 +16,14 @@ INSERT INTO `doctor` (`id`, `first_name`, `last_name`) VALUES
 (1, 'John', 'Dollittle'),
 (2, 'John', 'Doe');
 
+--
+-- Zrzut danych tabeli `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `appointment_date_and_time`, `customer_id`, `doctor_id`) VALUES
+(1, '2021-01-03 02:35:58', 1, 1),
+(2, '2021-01-03 03:35:00', 2, 1);
+
+
 -- --------------------------------------------------------
 
---
--- Struktura tabeli dla tabeli `hibernate_sequence`
---
-
-CREATE TABLE `hibernate_sequence` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Zrzut danych tabeli `hibernate_sequence`
---
-
-INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-(3);
-
---
--- Indeksy dla zrzutów tabel
---
-
---
--- Indeksy dla tabeli `appointment`
---
-ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKmyowslj1th8d9j6j3wlbwrtoe` (`customer_id`),
-  ADD KEY `FKoeb98n82eph1dx43v3y2bcmsl` (`doctor_id`);
-
---
--- Indeksy dla tabeli `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeksy dla tabeli `doctor`
---
-ALTER TABLE `doctor`
-  ADD PRIMARY KEY (`id`);
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `appointment`
---
-ALTER TABLE `appointment`
-  ADD CONSTRAINT `FKmyowslj1th8d9j6j3wlbwrtoe` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
-  ADD CONSTRAINT `FKoeb98n82eph1dx43v3y2bcmsl` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
